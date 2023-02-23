8df2ec8f983fdfbd5657010e97603c52c7b4a9b6
Katya <www@mail.ru>
2023-02-23 17:44:24 +0300

    Done

---
+++ Homework2.py
@@ -0,0 +1,31 @@
+
+#n = int(input("Введите количество монеток:"))       
+#a = 0
+#b = 0
+#for i in range (n):
+    #count = int(input())
+    #if count == 0:
+       #a+=1
+    #else: b+=1
+    #if a > b:
+        #print(a)
+    #else:
+        #print(b)
+        
+#print("Введите число:")
+#x = int(input())
+#y = int(input())
+#for i in range (x):
+    #for k in range (y):
+        #if x == i + k and y == i * j
+        #print(i, k)
+
+
+ 
+#m  =int(input())
+#k = 0
+#while 2**k <=m:
+    #print(2**k)
+    #k+=1
+                                                                                              
+
